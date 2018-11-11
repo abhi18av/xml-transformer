6b1cecca345f205e42103b53807f671c0e6f1f68
Abhinav Sharma <abhi18av@users.noreply.github.com>
2018-11-11 16:16:53 +0530

    Update project.clj

--- project.clj
+++ project.clj
@@ -3,7 +3,31 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [
+                  
+                  [org.clojure/clojure "1.9.0"]
+                 
+                  [org.clojure/data.xml "0.0.8"]
+                 
+                  ;[org.clojure/data.xml "0.2.0-alpha5"]
+                 [org.clojure/data.zip "0.1.2"]
+
+
+                 [com.github.kyleburton/clj-xpath "1.4.11"]
+                 
+                 [tolitius/xml-in "0.1.0"]
+
+                 ;; String manipulation
+                 [funcool/cuerdas "2.0.5"]
+
+
+                 ;; Clojure Utility Belt
+                 [com.rpl/specter "1.0.3"]
+                 [medley "1.0.0"]
+
+
+                 
+                 ]
   :main ^:skip-aot xml-transformer.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
